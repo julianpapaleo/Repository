765b12e93bfd5bcc9e81c11af3a604691de66a62
julianpapaleo <jpapaleo@itba.edu.ar>
2023-05-30 20:23:17 -0300

    30/05

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+let email = prompt("Ingresa tu dirección de correo electrónico:");
+
+if (email !== null && email !== "") {
+ 
+  console.log("Has ingresado el correo electrónico: " + email);
+} else {
+  console.log("No has ingresado un correo electrónico.");
+}
